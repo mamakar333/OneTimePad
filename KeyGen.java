1af73634c4a0dea80dae9a4a9ee253baa4c3b462
Your Name <you@example.com>
2018-09-19 19:38:52 -0400

    Encryption Success

--- KeyGen.java
+++ KeyGen.java
@@ -6,35 +6,46 @@ class KeyGen {
 
   public static void main(String[] args) {
 
-    String str = "00000000000000000000000000000000";
+    String str = "00000000";
     char[] charArray = str.toCharArray();
-    System.out.println(charArray);
+    //System.out.println(charArray);
 
     //charArray[31]='1';
+    String str1 = "beat";
+    char[] message=str1.toCharArray();
+    String binaryMessage="";
+  //  int num1 = message[0];
+  //  System.out.println("The value of fisrt digit "+b);
 
     int num = 10;
-    System.out.println(charArray);
+  //  System.out.println(charArray);
     int num1 = 2147483647,pol=0;
     int count=0;
-    int pl=31;
-    while( num1 !=1)
-    {
-      pol=num1%2;
-      if(num1%2==1)
-      {
-        charArray[pl]='1';
-      }
-      num1 = num1/2;
-      pl--;
-      count--;
-      System.out.println("This is ..."+num1+" The reminder "+pol);
+    int pl;
 
+    for(int i=0;i<str1.length();i++)
+    {
+      pl=7;
+      num1 = message[i];
+      System.out.println(num1);
+      while( num1 !=1)
+      {
+        pol=num1%2;
+        if(num1%2==1)
+        {
+          charArray[pl]='1';
+        }
+        num1 = num1/2;
+        pl--;
+
+      }
+      charArray[pl]='1';
+      System.out.println(charArray);
     }
-    charArray[pl]='1';
     count=count+1;
 
-    System.out.println("THe count is "+count);
-    System.out.println(charArray);
+  //  System.out.println("THe count is "+count);
+  //  System.out.println(charArray);
 
 
 
--- OneTimePad.java
+++ OneTimePad.java
@@ -7,14 +7,23 @@ class OneTimePad
   public static void main(String[] args)throws Exception
   {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the value of lamda : ");
+    System.out.print("Enter the value of lamda : ");
     int numBits = sc.nextInt();
     System.out.print("Enter a 4 letter message to be encoded :");
     String msg = sc.next();
     System.out.println("The given message is "+msg);
     keyGenerator();
-    encryptionMaxima();
+    encryptionMaxima(msg);
   }
+
+
+  public static void decryptionStealth()
+  {
+
+
+  }
+
+
 
 
   public static int randomNumberInRange(int min, int max)
@@ -23,24 +32,20 @@ class OneTimePad
     return random.nextInt((max - min) + 1) + min;
   }
 
-  public static void encryptionMaxima()
+
+
+  public static void encryptionMaxima(String s)
   {
-
-    String str1 = "beat";
+    String key="";
+    String str1 = s;
     char[] message=str1.toCharArray();
-
-
     String binaryMessage="";
-  //  int num1 = message[0];
-  //  System.out.println("The value of fisrt digit "+b);
-
     for(int i=0;i<str1.length();i++)
     {
       int num1=message[i];
-      System.out.println("number is "+num1);
       String str = "00000000";
       char[] charArray1 = str.toCharArray();
-      System.out.println(charArray1);
+
       int count=0;
       int pl=7;
       while( num1 !=1)
@@ -52,15 +57,80 @@ class OneTimePad
         num1 = num1/2;
         pl--;
         count--;
-        System.out.println("The ......."+charArray1);
       }
       charArray1[pl]='1';
       count=count+1;
-      System.out.println("The digit is "+num1+" The binary = "+charArray1);
-      binaryMessage= binaryMessage+charArray1;
+      binaryMessage= binaryMessage+ new String(charArray1);
 
     }
-    System.out.println("The converted Binary message = "+binaryMessage);
+    try
+    {
+      PrintWriter writer = new PrintWriter("plaintext.txt");
+      writer.println(binaryMessage);
+      writer.close();
+    }
+    catch(Exception ex)
+    {
+      System.out.println(ex);
+    }
+
+
+    System.out.println("The plain text is : ");
+    System.out.println(binaryMessage);
+    String fileName="key.txt";
+    // Reading the Key from the txt file
+    try
+    {
+      BufferedReader in = new BufferedReader(new FileReader(fileName));
+      key = in.readLine();
+
+
+    }
+    catch(FileNotFoundException ex)
+    {
+      System.out.println("Unable to open file '" + fileName + "'");
+    }
+    catch(IOException ex)
+    {
+      System.out.println("Error reading file '" + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+    }
+    char[] keyArray = key.toCharArray();
+    char[] plainArray=binaryMessage.toCharArray();
+    char[] cipherArray= new char[32] ;
+    for(int k=0;k<32;k++)
+    {
+
+        if(keyArray[k]=='0' && plainArray[k]=='0')
+        {
+          cipherArray[k]='0';
+        }
+        if(keyArray[k]=='0' && plainArray[k]=='1')
+        {
+          cipherArray[k]='1';
+        }
+        if(keyArray[k]=='1' && plainArray[k]=='0')
+        {
+          cipherArray[k]='1';
+        }
+        if(keyArray[k]=='1' && plainArray[k]=='1')
+        {
+          cipherArray[k]='0';
+        }
+    }
+    System.out.println("The Cipher Array is :");
+    System.out.println(cipherArray);
+    try
+    {
+      PrintWriter writer = new PrintWriter("ciphertext.txt");
+      writer.println(cipherArray);
+      writer.close();
+    }
+    catch(Exception ex)
+    {
+      System.out.println(ex);
+    }
 
   }
 
@@ -87,6 +157,7 @@ class OneTimePad
     charArray[pl]='1';
     count=count+1;
 
+    System.out.println("The Random key is : ");
     System.out.println(charArray);
     try
     {
@@ -98,10 +169,5 @@ class OneTimePad
     {
       System.out.println(ex);
     }
-
   }
-
-
-
-
 }
